30b823980584530b498c9374248997c87d356afe
kvadrick <nmsmet@ya.ru>
2022-07-06 09:24:47 +0300

    Add folder 001_Hw

---
+++ 001_Hw/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число A");
+double A = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите степень B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double C = 1;
+
+for (int i = 1; i <= B; i++)
+{
+    C = C * A;
+}
+
+Console.WriteLine(C);
