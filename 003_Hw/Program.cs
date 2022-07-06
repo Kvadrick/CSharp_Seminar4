852415dad0988829632da86c83f9ef712bf2cf2a
kvadrick <nmsmet@ya.ru>
2022-07-06 09:25:27 +0300

    Add folder 003_Hw

---
+++ 003_Hw/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write("{0} ", col[position]);
+        position++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+
+
