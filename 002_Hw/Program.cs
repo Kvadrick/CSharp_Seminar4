c6e7f2033412cfd0c4fd62868c254f18e7df0d68
kvadrick <nmsmet@ya.ru>
2022-07-06 09:25:07 +0300

    Add folder 002_Hw

---
+++ 002_Hw/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (A > 0)
+{
+    sum = sum + A % 10;
+    A = A / 10;
+
+}
+Console.WriteLine(sum);
